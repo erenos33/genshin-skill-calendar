63271495ff90d551d92da8f7ea28b8a21d70de6f
RYUSEULKI <erenos33@gmail.com>
2025-04-12 19:14:47 +0900

    update

--- src/main/java/me/genshin/skillcalendar/controller/HomeController.java
+++ src/main/java/me/genshin/skillcalendar/controller/HomeController.java
@@ -1,14 +1,15 @@
 package me.genshin.skillcalendar.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.result.view.RedirectView;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
-    public RedirectView redirectToIndex() {
-        return new RedirectView("/index.html");
+    public String index() {
+        return "forward:/index.html";
     }
 }
